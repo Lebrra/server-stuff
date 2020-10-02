6b83ce03422f4775b6d7d8f3d69847c13192bebf
Nick <nickthwang@gmail.com>
2020-10-02 11:01:10 -0500

    new files

--- Server Files/server2.js
+++ Server Files/server2.js
@@ -1,3 +1,5 @@
+const { Console } = require('console');
+
 // JavaScript source code
 var app = require('express')();
 var server = require('http').Server(app);
@@ -9,6 +11,12 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+var Users = new Map(); // declaring our Users structure
+
+// <socketid, { 
+//     username, socketid, playState
+// }>, <key, value>
+
 io.sockets.on('connection', (socket) => {
     console.log('a user connected');
 
@@ -16,14 +24,43 @@ io.sockets.on('connection', (socket) => {
         id: socket.id
     });
 
+    // add new user to User Map
+    addUser(socket);
+
     socket.on('buttonClicked', (number) => {
         console.log('button pressed ' + number);
-        io.emit('serverMessage', {message: `button ${number}`});
+        io.emit('serverMessage', { message: `button ${number}` });
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
-    });                                                          
+        removeUser(socket);
+    });
+
+    function addUser(socket) {
+        // create a new user mapping
+        if (!Users.has(socket.id)) {
+            Users.set(
+                socket.id,
+                {
+                    username: "----",
+                    id: socket.id
+                }
+            );
+            checkUsers();
+        }
+    };
+
+    function removeUser(socket){
+        if (Users.has(socket.id)) {
+            Users.delete(socket.id);
+            checkUsers();
+        }
+    }
+
+    function checkUsers(){
+        console.table(Users);
+    }
 });
 
 server.listen(PORT);

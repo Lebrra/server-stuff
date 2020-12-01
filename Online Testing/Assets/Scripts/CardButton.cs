01c5cb61211ea2f6338c1f4a8aa00ad5fbdad4b7
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-12-01 16:17:39 -0600

    textmeshpro

--- Online Testing/Assets/Scripts/CardButton.cs
+++ Online Testing/Assets/Scripts/CardButton.cs
@@ -4,26 +4,60 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class CardButton : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler
+public class CardButton : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Card myCard;
 
+    Transform parentObject;
     Transform handObject;
+
+    bool waitForClick = false;
+
+    private void Update()
+    {
+        if(waitForClick && Input.GetMouseButtonDown(0))
+        {
+            ShrinkCard();
+        }
+    }
 
     public void MyCard(string cardName)
     {
         myCard = CardParser.parseCard(cardName);
-        handObject = transform.parent;
+        handObject = parentObject = transform.parent;
+
+        //set texts
+        char cardText = valueToChar(myCard.number);
+        if (cardText == '0') transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = "10";
+        else transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = cardText.ToString();
+        transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = myCard.suit.ToString();
+
+        if(myCard.suit == Suit.Diamond || myCard.suit == Suit.Heart)
+        {
+            // make texts red
+            transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().color = Color.red;
+            transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().color = Color.red;
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        // zoom in on card
+        if (!waitForClick)
+        {
+            transform.localScale *= 2;
+            Debug.Log("card expanded");
+
+            waitForClick = true;
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        transform.SetParent(handObject.parent);
+        GetComponent<Image>().raycastTarget = false;
+        transform.SetParent(parentObject.parent);
+
+        // if in drop handler, remove it from its list
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -32,5 +66,61 @@ public class CardButton : MonoBehaviour, IPointerClickHandler, IBeginDragHandler
         transform.position = Input.mousePosition;
     }
 
-    //public void 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GameObject dropObject = eventData.pointerCurrentRaycast.gameObject;
+        GetComponent<Image>().raycastTarget = true;
+
+        if (dropObject?.GetComponent<DropHandler>())
+        {
+            if (!dropObject.GetComponent<DropHandler>().checkValidDrop(this))
+            {
+                parentObject = dropObject.transform;
+                transform.SetParent(parentObject);
+            }
+            else
+            {
+                ReturnToHand();
+            }
+        }
+        else
+        {
+            ReturnToHand();
+        }
+    }
+
+    void ReturnToHand()
+    {
+        parentObject = handObject;
+        transform.SetParent(parentObject);
+    }
+
+    void ShrinkCard()
+    {
+        transform.localScale /= 2;
+        Debug.Log("card shrunk");
+        waitForClick = false;
+    }
+
+    char valueToChar(int num)
+    {
+        switch (num)
+        {
+            case 0: return 'J';
+            case 1: return 'A';
+            case 2: return '2';
+            case 3: return '3';
+            case 4: return '4';
+            case 5: return '5';
+            case 6: return '6';
+            case 7: return '7';
+            case 8: return '8';
+            case 9: return '9';
+            case 10: return '0';
+            case 11: return 'J';
+            case 12: return 'Q';
+            case 13: return 'K';
+            default: return '0';
+        }
+    }
 }
